c4b4984c5a5f1d52444aa9fcdc45c673f49596b1
Jan Jaap <janjaap@siewers.org>
2025-04-02 15:33:00 +0200

    first

---
+++ main.py
@@ -0,0 +1,3 @@
+import mijnmodule
+
+print(mijnmodule.vermenigvuldig(2, 3))
---
+++ mijnmodule.py
@@ -0,0 +1,9 @@
+
+
+def telop(getal1, getal2):
+    a = getal1 + getal2
+    return a
+    
+def vermenigvuldig(getal1, getal2):
+    a = getal1 * getal2
+    return a
